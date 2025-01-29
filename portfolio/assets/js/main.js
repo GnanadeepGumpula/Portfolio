5e2a42f3081e5e443f25d93ca8da271489c4b6e5
Gnanadeep Gumpula <gumpulagnanadeep@gmail.com>
2025-01-30 00:53:23 +0530

    Update main.js

--- portfolio/assets/js/main.js
+++ portfolio/assets/js/main.js
@@ -292,3 +292,18 @@ sr.reveal(`.footer, footer__container`, {
   distance: "30px",
 });
 
+/* project */
+
+var swiper = new Swiper('.blog-slider', {
+  spaceBetween: 30,
+  effect: 'fade',
+  loop: true,
+  mousewheel: {
+    invert: false,
+  },
+  // autoHeight: true,
+  pagination: {
+    el: '.blog-slider__pagination',
+    clickable: true,
+  }
+});
